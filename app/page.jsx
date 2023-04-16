39f3924f8059b9bb776b32b1e92afbf944437c5b
paulweezydesign <86060999+paulweezydesign@users.noreply.github.com>
2023-04-16 17:33:51 +0000

    3 files modified

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,8 @@
-const Home = () => {
+export default async function Home()  {
   return (
     <div style={{ fontFamily: 'sans-serif' }}>
       <h1 className="text-7xl font-black">Home</h1>
     </div>
   );
 };
-export default Home;
+
